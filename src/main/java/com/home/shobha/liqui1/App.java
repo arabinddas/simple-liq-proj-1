9166729f838ac94522da5547dd52579243a75d06
Arabind Das <arabinddas@Arabinds-iMac.local>
2025-09-22 19:38:53 +0530

    a simple liquibase project

---
+++ src/main/java/com/home/shobha/liqui1/App.java
@@ -0,0 +1,13 @@
+package com.home.shobha.liqui1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
